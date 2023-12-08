b9fef1068b5b394c743a790ac9be52c87e69849b
priyalagade <106811758+priyalagade@users.noreply.github.com>
2023-12-08 09:22:25 +0000

    datatype conversions

---
+++ basics/03_conversion.js
@@ -0,0 +1,28 @@
+//let score="33";
+//let score="33ass"//nan not a number
+//let score=null;//zero
+//let score=undefined;//nan
+//let score=true;
+let score="priya";
+
+//console.log(typeof score);
+
+let numberValue=Number(score);
+//console.log(typeof numberValue);//number
+//console.log(numberValue);//nan,0,nan
+
+//let state=1;
+//console.log(typeof(state));
+
+//let state=""//false
+let state="priya"//true
+let booleanState=Boolean(state);
+//console.log(typeof booleanState);
+//console.log(booleanState);
+
+let num=33;
+console.log(num);
+console.log(typeof num);
+let stringnum=String(num);
+console.log(stringnum);
+console.log(typeof stringnum);
